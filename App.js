e0bdd24d2c2d1a3b5b091b88d9de9dde07321361
MAHIWIZARD03 <104820287+MAHIWIZARD03@users.noreply.github.com>
2024-02-16 21:02:09 +0530

    push

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import 'tailwindcss/tailwind.css'
+import tw from 'twrnc';
+
+// const MyComponent = () => (
+//   <View style={tw`bg-blue-100`}>
+//     <Text style={tw.style('text-md', invalid && 'text-red-500')}>Hello</Text>
+//   </View>
+// );
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`bg-blue-100 `}>
+    <Text style={tw`justify-center flex bg-red-500 items-center w-screen h-screen`}>Gramart</Text>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
